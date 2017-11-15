e006cb67f8e0d0f32109d2fbd31aa163cbcbce1b
Sydney <i7658072@bournemouth.ac.uk>
2017-11-15 23:33:01 +0000

    added include/Image.h

---
+++ WhiteRedChess/include/Image.h
@@ -0,0 +1,41 @@
+#ifndef IMAGE_H_
+#define IMAGE_H_
+
+#include <memory>
+
+ class Image
+ {
+ private:
+     size_t WIDTH;
+     size_t HEIGHT;
+     size_t pixelDepth;
+
+     unsigned char *m_data;
+
+ public :
+
+//constructor
+     Image(size_t _width, size_t _height, size_t _depth = 3)
+     {
+        WIDTH = _width;
+        HEIGHT = _height;
+        pixelDepth = _depth;
+
+        int imageSize  = WIDTH * HEIGHT * pixelDepth;
+
+        m_data = new unsigned char[imageSize];
+
+        //std::unique_ptr<unsigned char[]> image(new unsigned char[imageSize]);
+     }
+
+     bool save (std::string _fname);
+
+     void setPixel (size_t _x, size_t _y, unsigned char _r, unsigned char _g, unsigned char _b);
+
+     void clearScreen (unsigned char _r10, unsigned char _g, unsigned char _b);
+
+ };
+
+
+
+#endif
